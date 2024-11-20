54989ed6850f22980cd49f4e0a7d2aec0ba31e28
Abdul <abdulgzia@gmail.com>
2024-11-19 19:22:32 -0500

    basic stuff about state machine

---
+++ src/main/java/org/firstinspires/ftc/teamcode/Event.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+public interface Event {
+    public MotorMovement run();
+
+    public void is_done();
+}
---
+++ src/main/java/org/firstinspires/ftc/teamcode/MotorMovement.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+public class MotorMovement {
+    double leftFrontPower;
+    double leftBackPower;
+    double rightFrontPower;
+    double rightBackPower;
+
+    double armPower;
+    double armRotationPower;
+
+    double intakePower;
+}

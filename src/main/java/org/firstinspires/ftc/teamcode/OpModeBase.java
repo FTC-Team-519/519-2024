2cc7e9194fd7dc4501121a60d29439bccd717371
Abdul <abdulgzia@gmail.com>
2024-12-03 20:56:49 -0500

    added code for deadzone and smoothsteping

--- src/main/java/org/firstinspires/ftc/teamcode/OpModeBase.java
+++ src/main/java/org/firstinspires/ftc/teamcode/OpModeBase.java
@@ -62,6 +62,28 @@ public abstract class OpModeBase extends OpMode {
         return false;
     }
 
+    static protected double deadzoneAndSmoothstep(double raw_value, double limit, double steepness){
+        if (Math.abs(raw_value) < limit){
+            return 0.0d;
+        }
+        double output = (Math.abs(raw_value)-limit) / (1.0d - limit);
+        double TwoPowerSteepness = Math.pow(2, steepness);
+        double denom = TwoPowerSteepness*Math.pow(0.5, steepness + 1) + 0.5d;
+        if (output>1.0){
+            return 1.0d;
+        }
+        if (output<0.5){
+            output = TwoPowerSteepness*Math.pow(output, steepness + 1)/denom;
+        }else{
+            output = 1.0d + TwoPowerSteepness*(output-1)*Math.pow(1-output, steepness)/denom;
+        }
+        Math.min(1.0d, Math.max(0.0d, output));
+        if (raw_value<0) {
+            output *= -1;
+        }
+        return output;
+    }
+
     protected boolean allTargetPositionsSet(int position) {
         return(frontLeft.getTargetPosition()==position && backLeft.getTargetPosition()==position &&
                 frontRight.getTargetPosition()==position && backRight.getTargetPosition()==position);
